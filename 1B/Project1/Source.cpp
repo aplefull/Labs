34f1d4d514b39af05b3eed10a9b84b0e373aa75f
aplefull <mraplefull@yandex.by>
2018-12-13 00:49:36 +0300

    1-4 fixed

--- 1B/Project1/Source.cpp
+++ 1B/Project1/Source.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <math.h>
+
 using namespace std;
 
 int main() {
-	double s, x, y, z, a, b;
+	double x, y, z, a, b;
 		x = 17.421;
 		y = 10.365E-3;
 		z = 0.828E5;
 		a = pow(y + pow(x - 1, 1 / 3.), 1 / 4.);
 		b = fabs(x - y)*(sin(z)*sin(z) + tan(z));
-			s = a / b;
-			cout << s << endl;
+
+		cout << a / b << endl;
 	return 0;
 }
